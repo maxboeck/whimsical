e015e3b4e32c7923a4bfa2c8daaf269a20410cdb
Max Böck <hello@mxb.at>
2020-06-30 17:32:02 +0200

    remove manifest

--- .eleventy.js
+++ .eleventy.js
@@ -50,7 +50,6 @@ module.exports = function (config) {
 
     // Pass-through files
     config.addPassthroughCopy('src/robots.txt')
-    config.addPassthroughCopy('src/site.webmanifest')
     config.addPassthroughCopy('src/assets/images')
     config.addPassthroughCopy('src/assets/fonts')
     config.addPassthroughCopy('src/assets/audio')
